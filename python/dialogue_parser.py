dfe5030953ec8277f1c0f9adb53c9b562cc45e7d
kocsisakos <kocsis.akos@students.jedlik.eu>
2023-11-01 17:00:18 +0100

    dialogue parser

--- python/dialogue_parser.py
+++ python/dialogue_parser.py
@@ -4,11 +4,9 @@ with open("dialogues.json", "r", encoding="utf-8") as f:
     dialogues = json.load(f)
 
 
-def read(input: list): #input = ["meghalt"]["ehseg"]
+def read(input: str): #input = "meghalt ehseg"
     output = dialogues
-    for key in input:
-        output= output[key]
+    x= input.split(" ")
+    for key in x:
+        output = output[key]
     return output
-
-# x= 3456
-# print(str(read(['meghalt', 'ehseg'])).format(x=x))
