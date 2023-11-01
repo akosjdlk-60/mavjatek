9a47022e41eb419609f351f55decac4dbbfd02ed
kocsisakos <kocsis.akos@students.jedlik.eu>
2023-11-01 17:00:37 +0100

    minden ami idovel kapcsolatos xd

--- python/ido.py
+++ python/ido.py
@@ -3,12 +3,23 @@ from asyncio import sleep
 
 async def clock():
     global ido
-    ido = datetime.time(8,0)
+    ido = datetime.datetime(year=2023, month=1, day=1, hour=8, minute=0)
     while True:
-        sleep(1)
+        await sleep(1)
         ido += datetime.timedelta(minutes=1)
 
 def time():
     return ido
+
+def set_time(hour:int, minute:int):
+    global ido
+    ido = datetime.datetime(year=ido.year, month=ido.month, day=ido.day, hour=hour, minute=minute)
+
+
+def add_time(change: int):
+    """
+    change: Hány perc időt adjunk hozzá.
+    """
+    global ido
+    ido += datetime.timedelta(minutes=change)    
     
-        
