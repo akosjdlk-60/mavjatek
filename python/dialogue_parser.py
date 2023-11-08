7068989c342e2a9e9d8cd49ae2a286161d27aeb9
kocsisakos <kocsis.akos@students.jedlik.eu>
2023-11-08 11:32:23 +0100

    allomas menu kesz

--- python/dialogue_parser.py
+++ python/dialogue_parser.py
@@ -2,7 +2,7 @@ import json
 import os
 
 path = f"{os.path.abspath(os.getcwd())}\\"
-with open(f"{path}dialogues.json", "r", encoding="utf-8") as f:
+with open(f"{path}python\\dialogues.json", "r", encoding="utf-8") as f:
     dialogues = json.load(f)
 
 
